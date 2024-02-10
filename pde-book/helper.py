9c61d49eb76797f1060b1eeb2b9278591c9fd770
Carlos Aznarán <caznaranl@uni.pe>
2024-02-09 22:33:04 -0500

    Update

--- pde-book/helper.py
+++ pde-book/helper.py
@@ -1,3 +1,6 @@
+from sympy import Eq
+
+
 def cleaner(symbolsToDelete: list):
     """
     Helper function that call the garbage collector (clean the symbol's mess).
@@ -7,3 +10,8 @@ def cleaner(symbolsToDelete: list):
             del globals()[z0]
     except KeyError:
         print("Symbolic variables already cleared")
+
+
+class Approx(Eq):
+    def _latex(self, printer):
+        return rf"{printer._print(self.lhs)} \approx {printer._print(self.rhs)}"
